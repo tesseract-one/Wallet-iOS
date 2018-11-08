1628099419b06b4cd7c21b38ddd2b285b30e4c58
Yehor Popovych <ypopovych@crossroad.io>
2018-11-08 14:10:14 +0200

    Initial Commit

---
+++ Tesseract/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Tesseract
+//
+//  Created by Yehor Popovych on 11/8/18.
+//  Copyright © 2018 Crossroad Labs s.r.o. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Tesseract/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Tesseract
+//
+//  Created by Yehor Popovych on 11/8/18.
+//  Copyright © 2018 Crossroad Labs s.r.o. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
