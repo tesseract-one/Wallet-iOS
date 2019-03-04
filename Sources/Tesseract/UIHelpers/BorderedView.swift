cbddb5985cf149f5c8e6ceda0bb12dc124bc001f
Yura <yura.kulinich.ua@gmail.com>
2019-03-04 23:40:59 +0200

    Added UI to ScanQR screen

---
+++ Sources/Tesseract/UIHelpers/BorderedView.swift
@@ -0,0 +1,39 @@
+//
+//  BorderedView.swift
+//  Tesseract
+//
+//  Created by Yura Kulynych on 3/4/19.
+//  Copyright © 2019 Crossroad Labs s.r.o. All rights reserved.
+//
+
+import UIKit
+
+class BorderedView: UIView {
+    
+    @IBInspectable override var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = self.borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = self.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = self.cornerRadius
+            layer.masksToBounds = self.cornerRadius > 0
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+}
